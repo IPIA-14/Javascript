f4653a257e37b921e2b591c7a8253046529dcaa6
IPIA-14 <ipiajuanjo7@gmail.com>
2025-05-07 10:23:29 -0500

    Ejercicios Javascript

--- Introduccion/Ejercicio10.js
+++ Introduccion/Ejercicio10.js
@@ -1,4 +1,11 @@
 function Numeros(){
     let valor1 = parseInt(document.getElementById("input1").value);
-    let parImpar = valor1 % 2 
+    let parImpar = valor1 % 2 === 0;
+    
+    document.getElementById("mostrar").innerHTML = "El numero " + valor1 + " es " + parImpar
+
+    const botonRecargar = document.getElementById("botonReiniciar");
+    botonRecargar.addEventListener("click", () => {
+        location.reload(); 
+    });
 }
