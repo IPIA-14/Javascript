25b2d8626dfc12719026eba7cf777290145a22d6
IPIA-14 <ipiajuanjo7@gmail.com>
2025-05-11 22:26:57 -0500

    Primeros Ejercicios JavasCript RESTAURADOS

---
+++ Introduccion/Ejercicio1.js
@@ -0,0 +1,23 @@
+
+let nombre = "Juan Ipia";
+document.getElementById("mostrar").innerHTML =
+ "Hola " + nombre + " vamos a calcular cuanto pagaras hoy, ingresa los datos correspondientes."
+
+function pagar(){
+
+    let valor1 = parseInt(document.getElementById("input1").value);
+    let valor2 = parseInt(document.getElementById("input2").value);
+    let iva = valor1 * valor2 / 100
+    let resultado = valor1 + iva;
+
+    document.getElementById("mostrar").innerHTML = "El total que usted debe pagar por el producto es de: $" + resultado + "<br> Usted pagara por el producto un total de: $" + valor1 + "<br> Usted pagara por el iva un total de: $" + iva
+
+    const botonRecargar = document.getElementById("botonReiniciar");
+    botonRecargar.addEventListener("click", () => {
+        location.reload(); 
+    });
+}
+
+
+
+
---
+++ Introduccion/Ejercicio4.js
@@ -0,0 +1,14 @@
+function media(){
+    let valor1 = parseInt(document.getElementById("input1").value);
+    let valor2 = parseInt(document.getElementById("input2").value);
+    let valor3 = parseInt(document.getElementById("input3").value);
+    let media = (valor1 + valor2 + valor3) / 3
+
+    document.getElementById("mostrar").innerHTML = "El resultado de la media es: " + media
+
+    const botonRecargar = document.getElementById("botonReiniciar");
+    botonRecargar.addEventListener("click", () => {
+        location.reload(); 
+    });
+}
+
