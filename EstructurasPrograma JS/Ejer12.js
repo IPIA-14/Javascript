e2cf9a5ef3759b9966d80c5b7701bd95a96bd47c
IPIA-14 <ipiajuanjo7@gmail.com>
2025-05-11 22:34:07 -0500

    Primeros Ejercicios JavasCript RESTAURADOS

--- EstructurasPrograma JS/Ejer12.js
+++
@@ -1,5 +0,0 @@
-let sacar=""
-for (let i = 1; i <= 5 ; i ++){
-    sacar= sacar + "*"
-    console.log(sacar)
-}
--- EstructurasPrograma JS/Ejer2.js
+++
@@ -1,9 +0,0 @@
-let años = 18
-let cedula = "si"
-let CCinscrita = "si"
-
-if (años >= 18 && cedula === "si" &&CCinscrita === "si"){
-    console.log("Usted puede votar")
-}else{
-    console.log("Usted NO puede votar")
-}
